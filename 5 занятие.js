3a4c4259b221a36cead39d537b3aeec60508ac2b
ilogic-9-p <gosabalabanov@gmail.com>
2024-10-28 15:55:44 +0300

    изучали css 3

---
+++ 5 занятие.js
@@ -0,0 +1,9 @@
+let a = 1;
+let b = "sadasdfs";
+let c = true;
+let d = null;
+let t = a;
+let r = a +t;
+console.log("Здесь я проверю значения в своих данных");
+console.log(r)
+console.log(r, a, t, d)
