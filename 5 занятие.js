dfccb728d0917f0b374938f78d8fd8b980be6693
ilogic-9-p <gosabalabanov@gmail.com>
2024-10-30 18:00:51 +0300

    изучали джава скрипт

--- 5 занятие.js
+++ 5 занятие.js
@@ -1,9 +1,38 @@
-let a = 1;
-let b = "sadasdfs";
-let c = true;
-let d = null;
-let t = a;
-let r = a +t;
-console.log("Здесь я проверю значения в своих данных");
-console.log(r)
-console.log(r, a, t, d)
+// let a = 1;
+// let b = "sadasdfs";
+// let c = true;
+// let d = null;
+// let t = a;
+// let r = a + t;
+// console.log('Здесь я проверяю значения в своих данных');
+// console.log(r);
+//     console.log(r, a, t, d);
+// let hello = 'Привет, ';
+// let name = 'Павел';
+//     let st = hello + name;
+//     console.log(st);
+// console.log(hello + name);
+
+// console.log(a);
+// a = a + a;
+// a = a + a;
+// console.log(a);
+
+
+//let span = document.getElementById('answer');
+//span.innerText = 5;
+
+
+//let name = ("Укажите ваше имя")
+//console.log(name);
+
+//let a = prompt("Укажите первое слагаемое")
+//a = +a
+//let b = prompt("Укажите второе слагаемое")
+//b = +b
+//console.log(a, Number(a), +a)
+//console.log(1)
+//console.log("Сумма" , a + b )
+
+//let span = document.getElementById('answer');
+//span.innerText = a + b;
---
+++ Самостоятельная работа.js
@@ -0,0 +1,7 @@
+let nameDOM = document.getElementById('name');
+let y = prompt("Укажите свое имя")
+nameDOM.innerText = y;
+
+let surnameDOM = document.getElementById('surname');
+let w = prompt("Укажите свою фамилию")
+surnameDOM.innerText = w;
