762d16c154467a544d5a394c9a7eba472ff7cef6
ilogic-9-p <gosabalabanov@gmail.com>
2024-11-06 17:59:24 +0300

    изучали джава скрипт 2

--- Самостоятельная работа.js
+++ Самостоятельная работа.js
@@ -1,7 +1,37 @@
+
 let nameDOM = document.getElementById('name');
-let y = prompt("Укажите свое имя")
+let y = prompt("Укажите своё имя")
 nameDOM.innerText = y;
 
-let surnameDOM = document.getElementById('surname');
+let lastnameDOM = document.getElementById('lastname');
 let w = prompt("Укажите свою фамилию")
-surnameDOM.innerText = w;
+lastnameDOM.innerText = w;
+
+let midlenameDOM = document.getElementById('midlename');
+let q = prompt("Укажите своё отчество")
+midlenameDOM.innerText = q;
+
+let ageDOM = document.getElementById('age');
+let e = prompt("Укажите свой возраст")
+ageDOM.innerText = e;
+
+let schoolDOM = document.getElementById('school');
+let r = prompt("Укажите свою школу")
+schoolDOM.innerText = r;
+
+let classDOM = document.getElementById('class');
+let t = prompt("Укажите свой класс")
+classDOM.innerText = t;
+
+let sportDOM = document.getElementById('sport');
+let j = prompt("Укажите свой любимый вид спорта")
+sportDOM.innerText = j;
+
+let subjectDOM = document.getElementById('subject');
+let u = prompt("Укажите свой любимый предмет")
+subjectDOM.innerText = u;
+
+let colorDOM = document.getElementById('color');
+let i = prompt("Укажите свой любимый цвет")
+colorDOM.innerText = i;
+
