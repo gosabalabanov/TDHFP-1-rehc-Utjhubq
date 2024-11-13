c720abd7df23136b7a31a788cffd8d3c2e264e19
ilogic-9-p <gosabalabanov@gmail.com>
2024-11-13 18:01:30 +0300

    изучали джава скрипт 3

---
+++ 7 занятие.js
@@ -0,0 +1,10 @@
+// let div = document.getElementById('div');
+// console.log(div.innerText);
+// div.innerText = '123123'
+// console.log(div.innerText);
+//
+// let input = document.getElementById('input');
+// console.log(input.value);
+// input.value = '123123';
+// console.log(input.value);
+
